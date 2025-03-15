b65e0a4b88a5c80b60b06126eca90d0f7af556ff
Jackson <47034052@qq.com>
2025-03-15 20:49:02 +0800

    modify

--- main.cpp
+++ main.cpp
@@ -1,13 +1,7 @@
 #include "sort/quick_sort.h"
 
-void enterSpace() {
-    std::cout << std::endl;
-    std::cout << std::endl;
-}
 
 int main() {
-    enterSpace();
-
     vector<int> arr {1,4,3,6,5,3,1,2,10,9};
 
     QuickSort<int> sorter;
